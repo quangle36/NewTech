e719d9222e5181facf60b8dd272287a17e45943e
Quang Le <thanhtam8004@gmail.com>
2021-01-04 19:28:05 +0700

    add config

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  port: process.env.PORT || 8080,
+  database: "",
+  secret: "",
+  google: {
+    clientID:
+      "",
+  },
+  facebook: {
+    clientID: "",
+    clientSecret: "",
+  },
+};
